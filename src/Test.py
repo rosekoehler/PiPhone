23ab960f47d62d80e2542bac227739e6ae742306
rosekoehler <rosekoehler13@gmail.com>
2025-12-21 18:22:11 -0800

    testing

--- src/Test.py
+++ src/Test.py
@@ -3,13 +3,11 @@ import tkinter as tk
 # creates main window
 screen = tk.Tk()
 screen.title("My App")           # Set window title
-screen.resizable(False, False)   # Prevent resizing
-screen.minsize(300, 200)         # Set minimum size
-screen.maxsize(800, 600)         # Set maximum size
-
+screen.attributes("-fullscreen", True)
 # makes button
 # lamda is a quick way to write a small function
 # pack() puts it on the screen 
 tk.Button(screen, text="Click Me", command=lambda: print("Clicked!")).pack()
+
 
 screen.mainloop()
